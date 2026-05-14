611945eea132dceebcb2c6f19b2fca7a443f9c57
HPK017 <parelharsh0250@gmail.com>
2026-05-14 20:32:41 +0530

    patient-management-microservices - delete-patient-service

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -58,7 +58,9 @@ public class PatientService {
         return PatientMapper.toDTO(updatedPatient);
     }
 
-
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 
 
 }
